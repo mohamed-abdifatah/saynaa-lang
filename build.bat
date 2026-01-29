a1b89ba5adc26fb620caa6ef6d930e5582e665c2
Mohamed Abdifataax Jama <mahdiware05@gmail.com>
2026-01-29 11:39:12 +0300

    Refactor build.bat for improved readability and structure

--- build.bat
+++ build.bat
@@ -2,42 +2,33 @@
 :: Distributed Under The MIT License
 
 @echo off
-setlocal
-Pushd %cd%
-cd %~dp0
+setlocal enabledelayedexpansion
+pushd %~dp0
 
 :: Root directory of the project
-set project_root=%~dp0
+set "project_root=%~dp0"
 
 :: ----------------------------------------------------------------------------
 :: DEPENDENCIES
 :: ----------------------------------------------------------------------------
-set pcre2_path=%project_root%deps\pcre2
-set pcre2_inc=/I"%pcre2_path%\include"
-set pcre2_lib="%pcre2_path%\lib\pcre2-8-static.lib"
+set "pcre2_path=%project_root%deps\pcre2"
+set "pcre2_inc=/I"%pcre2_path%\include""
+set "pcre2_lib="%pcre2_path%\lib\pcre2-8-static.lib""
 
 :: ----------------------------------------------------------------------------
 :: PARSE COMMAND LINE ARGUMENTS
 :: ----------------------------------------------------------------------------
-
-set enable_debug=true
-set use_shared_lib=false
-
-goto :PARSE_ARGS
-
-:SHIFT_ARG_2
-shift
-:SHIFT_ARG_1
-shift
+set "enable_debug=true"
+set "use_shared_lib=false"
 
 :PARSE_ARGS
-if (%1)==(-h) goto :PRINT_USAGE
-if (%1)==(-c) goto :CLEAN
-if (%1)==(-r) set enable_debug=false && goto :SHIFT_ARG_1
-if (%1)==(-s) set use_shared_lib=true && goto :SHIFT_ARG_1
-if (%1)==() goto :CHECK_MSVC
+if "%~1"=="" goto :CHECK_MSVC
+if "%~1"=="-h" goto :PRINT_USAGE
+if "%~1"=="-c" goto :CLEAN
+if "%~1"=="-r" (set "enable_debug=false" & shift & goto :PARSE_ARGS)
+if "%~1"=="-s" (set "use_shared_lib=true" & shift & goto :PARSE_ARGS)
 
-echo Invalid argument "%1"
+echo Invalid argument "%~1"
 
 :PRINT_USAGE
 echo Usage: call build.bat [options ...]
@@ -52,141 +43,121 @@ goto :END
 :: INITIALIZE MSVC ENVIRONMENT
 :: ----------------------------------------------------------------------------
 :CHECK_MSVC
+if defined INCLUDE goto :START
 
-if not defined INCLUDE goto :MSVC_INIT
-goto :START
-
-:MSVC_INIT
 echo Not running on an MSVC prompt, searching for one...
 
-:: Find vswhere - Fixed quoting for paths with spaces
-if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
-    set "VSWHERE_PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-) else if exist "%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe" (
-    set "VSWHERE_PATH=%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
-) else (
-    echo Can't find vswhere.exe
+:: Find vswhere.exe safely
+set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "!vswhere!" set "vswhere=%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
+
+if not exist "!vswhere!" (
+    echo Error: can't find vswhere.exe
     goto :NO_VS_PROMPT
 )
 
-:: Get the VC installation path - Use "usebackq" to handle spaces in VSWHERE_PATH
-for /f "usebackq tokens=*" %%i in (`^"%VSWHERE_PATH%^" -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -latest -property installationPath`) do (
-    set "VS_INSTALL_PATH=%%i"
+:: Get the installation path using vswhere
+for /f "usebackq tokens=*" %%i in (`"!vswhere!" -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+    set "install_path=%%i"
 )
 
-if not exist "%VS_INSTALL_PATH%" (
+if not exist "!install_path!" (
     echo Error: can't find Visual Studio installation directory
     goto :NO_VS_PROMPT
 )
 
-echo Found at - %VS_INSTALL_PATH%
+echo Found at - "!install_path!"
 
-:: Initialize VC for X86_64 - Corrected path concatenation
-call "%VS_INSTALL_PATH%\VC\Auxiliary\Build\vcvars64.bat"
+:: Call vcvars64.bat to set up the environment
+set "vcvars=!install_path!\VC\Auxiliary\Build\vcvars64.bat"
+if not exist "!vcvars!" (
+    echo Error: can't find vcvars64.bat
+    goto :NO_VS_PROMPT
+)
+
+call "!vcvars!"
 if errorlevel 1 goto :NO_VS_PROMPT
 echo Initialized MSVC x86_64
 goto :START
 
 :NO_VS_PROMPT
-echo You must open a "Visual Studio .NET Command Prompt" to run this script
-goto :END
+echo You must open a "Visual Studio .NET Command Prompt" to run this script or ensure VS is installed.
+exit /b 1
 
 :: ----------------------------------------------------------------------------
-:: START
+:: START BUILD
 :: ----------------------------------------------------------------------------
 :START
-
 set "target_dir=%project_root%obj\"
-set additional_cflags=-W3 -GR /FS -EHsc
-set additional_linkflags=/SUBSYSTEM:CONSOLE
-set additional_defines=/D_CRT_SECURE_NO_WARNINGS /DPCRE2_STATIC
+set "add_cflags=-W3 -GR /FS -EHsc"
+set "add_linkflags=/SUBSYSTEM:CONSOLE"
+set "add_defines=/D_CRT_SECURE_NO_WARNINGS /DPCRE2_STATIC"
 
-if "%enable_debug%"=="false" (
-    set additional_cflags=%additional_cflags% -O2 -MD /DNDEBUG
+if "!enable_debug!"=="false" (
+    set "cflags=-O2 -MD /DNDEBUG"
 ) else (
-    set additional_cflags=%additional_cflags% -MDd -ZI
-    set additional_defines=%additional_defines% /DDEBUG
+    set "cflags=-MDd -ZI"
+    set "add_defines=!add_defines! /DDEBUG"
 )
 
-if "%use_shared_lib%"=="true" (
-    set additional_defines=%additional_defines% /D_DLL_ /D_COMPILE_
+if "!use_shared_lib!"=="true" (
+    set "add_defines=!add_defines! /D_DLL_ /D_COMPILE_"
 )
 
-:: Make intermediate folders.
-if not exist "%target_dir%" mkdir "%target_dir%"
-if not exist "%target_dir%lib\" mkdir "%target_dir%lib\"
-if not exist "%target_dir%saynaa\" mkdir "%target_dir%saynaa\"
-if not exist "%target_dir%cli\" mkdir "%target_dir%cli\"
+:: Create directories
+if not exist "!target_dir!" mkdir "!target_dir!"
+if not exist "!target_dir!lib\" mkdir "!target_dir!lib\"
+if not exist "!target_dir!saynaa\" mkdir "!target_dir!saynaa\"
+if not exist "!target_dir!cli\" mkdir "!target_dir!cli\"
 
 :: ----------------------------------------------------------------------------
-:: COMPILE CORE & OPTIONALS
+:: COMPILE CORE
 :: ----------------------------------------------------------------------------
-:COMPILE
-
-cd "%target_dir%saynaa"
-
-cl /nologo /c %additional_defines% %pcre2_inc% %additional_cflags% ^
-    "%project_root%src\compiler\*.c" ^
-    "%project_root%src\optionals\*.c" ^
-    "%project_root%src\runtime\*.c" ^
-    "%project_root%src\shared\*.c" ^
-    "%project_root%src\utils\*.c"
-
+cd /d "!target_dir!saynaa"
+cl /nologo /c !add_defines! !pcre2_inc! !add_cflags! !cflags! ^
+    "!project_root!src\compiler\*.c" ^
+    "!project_root!src\optionals\*.c" ^
+    "!project_root!src\runtime\*.c" ^
+    "!project_root!src\shared\*.c" ^
+    "!project_root!src\utils\*.c"
 if errorlevel 1 goto :FAIL
 
-if "%use_shared_lib%"=="true" (
-  set "mylib=%target_dir%bin\saynaa.lib"
+set "mylib=!target_dir!lib\saynaa.lib"
+if "!use_shared_lib!"=="true" (
+    set "mylib=!target_dir!bin\saynaa.lib"
+    if not exist "!target_dir!bin\" mkdir "!target_dir!bin\"
+    link /nologo /dll /out:"!target_dir!bin\saynaa.dll" /implib:"!mylib!" *.obj !pcre2_lib!
 ) else (
-  set "mylib=%target_dir%lib\saynaa.lib"
+    lib /nologo !add_linkflags! /OUT:"!mylib!" *.obj
 )
-
-if "%use_shared_lib%"=="true" goto :SHARED
-
-lib /nologo %additional_linkflags% /OUT:"%mylib%" *.obj
-goto :SRC_END
-
-:SHARED
-if not exist "%target_dir%bin\" mkdir "%target_dir%bin\"
-link /nologo /dll /out:"%target_dir%bin\saynaa.dll" /implib:"%mylib%" *.obj %pcre2_lib%
-
-:SRC_END
 if errorlevel 1 goto :FAIL
 
 :: ----------------------------------------------------------------------------
 :: COMPILE CLI
 :: ----------------------------------------------------------------------------
-cd "%target_dir%cli"
-
-cl /nologo /c %additional_defines% %pcre2_inc% %additional_cflags% "%project_root%src\cli\*.c"
+cd /d "!target_dir!cli"
+cl /nologo /c !add_defines! !pcre2_inc! !add_cflags! !cflags! "!project_root!src\cli\*.c"
 if errorlevel 1 goto :FAIL
-
-cd "%project_root%"
 
 :: Final Link
-cl /nologo %additional_defines% "%target_dir%cli\*.obj" "%mylib%" %pcre2_lib% /Fe"%project_root%saynaa.exe"
+cd /d "!project_root!"
+cl /nologo !add_defines! "!target_dir!cli\*.obj" "!mylib!" !pcre2_lib! /Fe"saynaa.exe"
 if errorlevel 1 goto :FAIL
 
-goto :SUCCESS
-
-:CLEAN
-if exist "%project_root%obj" rmdir /S /Q "%project_root%obj"
-if exist "%project_root%saynaa.exe" del "%project_root%saynaa.exe"
-echo.
-echo Files were cleaned.
-goto :END
-
-:SUCCESS
 echo.
 echo Compilation Success: saynaa.exe is ready.
 goto :END
 
+:CLEAN
+if exist "obj" rmdir /S /Q "obj"
+if exist "saynaa.exe" del "saynaa.exe"
+echo Files were cleaned.
+goto :END
+
 :FAIL
-popd
-endlocal
 echo Build failed.
 exit /b 1
 
 :END
 popd
 endlocal
-goto :eof
